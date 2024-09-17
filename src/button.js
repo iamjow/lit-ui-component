a4bc740527142be6cca166b66dfafcee2ac89a40
iamjow <jowin@qstrike.com>
2024-09-17 15:10:55 +0800

    lit components button

---
+++ src/button.js
@@ -0,0 +1,27 @@
+import { LitElement, html, css } from 'https://unpkg.com/lit@2.0.0?module'; // Ensure you're using a CDN link
+
+class MyButton extends LitElement {
+  static styles = css`
+    button {
+      padding: 10px 20px;
+      background-color: #6200ee;
+      color: white;
+      border: none;
+      border-radius: 5px;
+      cursor: pointer;
+    }
+    button:hover {
+      background-color: #3700b3;
+    }
+  `;
+
+  render() {
+    return html`<button @click=${this._handleClick}>Click Me</button>`;
+  }
+
+  _handleClick() {
+    alert('Button clicked!');
+  }
+}
+
+customElements.define('jowin-button', MyButton);
